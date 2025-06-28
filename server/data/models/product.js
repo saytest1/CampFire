2fd8a77c1f90f93c48301ce8f4ab53298c56917d
Peter Bao <94208129+vina123baov@users.noreply.github.com>
2025-06-28 05:02:25 +0000

    up

--- server/data/models/product.js
+++ server/data/models/product.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 let Schema = mongoose.Schema;
 let String = Schema.Types.String;
 let Number = Schema.Types.Number;
-let Float = Schema.Types.Float;
 
 export const ProductSchema = new Schema(
   {
     name: String,
-    price: Float,
+    price: Number,           // ✅ Sửa từ Float → Number
     categoryId: Number,
     manufacturerId: Number, 
   },
