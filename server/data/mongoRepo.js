d1af4ed3c7c1e90725b22ddf444a373f3a1bc342
SayHoang <sayhoang.work@gmail.com>
2025-07-04 22:33:47 +0700

    add filter & sort for categories and products

--- server/data/mongoRepo.js
+++ server/data/mongoRepo.js
@@ -5,12 +5,45 @@ import { Detail } from "./models/index.js";
 import { Order } from "./models/index.js";
 import { User } from "./models/index.js";
 
+const values = {
+  ASC: 1,
+  DESC: -1,
+};
+
+function buildOptions(choices, columns) {
+  const options = {};
+  choices.forEach((option) => { // ID_ASC
+    const [left, right] = option.split('_');
+    const key = columns[left];
+    const value = values[right];
+    options[key] = value;
+  });
+  return options;
+}
+
 const db = {
   // categories
   categories: {
-    getAll: async () => {
-      const items = await Category.find();
-      return items;
+    getAll: async ({ first, offset, orderBy }) => {
+      const query = {};
+
+      const columns = {
+        ID: "_id",
+        NAME: "name",
+      };
+
+      const options = buildOptions(orderBy, columns);
+
+      const totalCount = await Category.find(query).sort(options).countDocuments();
+      if (offset >= totalCount) {
+        offset = 0;
+      }
+
+      const items = await Category.find(query).sort(options).skip(offset).limit(first);
+      return {
+        items: items,
+        totalCount: totalCount,
+      };
     },
     create: async ({ name }) => {
       const created = await Category.create({
@@ -37,9 +70,38 @@ const db = {
 
   // products
   products: {
-    getAll: async () => {
-      const items = await Product.find();
-      return items;
+    getAll: async ({ first, offset, orderBy, condition }) => {
+      const query = {};
+
+      if (condition) {
+        if (condition.name) {
+          query.name = { $regex: condition.name, $options: "i" };
+        }
+
+        if (condition.price) {
+          query.price = { $gte: condition.price.min, $lte: condition.price.max };
+        }
+      }
+
+      const columns = {
+        ID: "_id",
+        NAME: "name",
+        PRICE: "price",
+      };
+
+      const options = buildOptions(orderBy, columns);
+
+      const totalCount = await Product.find(query).sort(options).countDocuments();
+      if (offset >= totalCount) {
+        offset = 0;
+      }
+
+      const items = await Product.find(query).sort(options).skip(offset).limit(first);
+
+      return {
+        items: items,
+        totalCount: totalCount,
+      };
     },
     create: async ({ name, price, categoryId, manufacturerId }) => {
       const created = await Product.create({
