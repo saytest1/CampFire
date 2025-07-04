133925e090a748f55b54709a15005c49e150397f
SayHoang <sayhoang.work@gmail.com>
2025-07-04 16:04:15 +0700

    add sample products & edit auth

--- server/data/models/user.js
+++ server/data/models/user.js
@@ -5,8 +5,18 @@ let String = Schema.Types.String;
 export const UserSchema = new Schema(
   {
     username: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ["admin", "manager", "customer"],
+        default: "customer",
+    },
   },
   {
-    collection: "users",
+    timestamps: true,
   }
 );
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
