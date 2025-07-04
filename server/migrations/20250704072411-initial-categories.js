93bb7dd0f0d701fc6ccb00f43da3af0d9c2c2b2a
SayHoang <sayhoang.work@gmail.com>
2025-07-04 14:33:59 +0700

    add initial categories and manufacturers

--- server/migrations/20250704072411-initial-categories.js
+++ server/migrations/20250704072411-initial-categories.js
@@ -1,24 +1,10 @@
-module.exports = {
-  /**
-   * @param db {import('mongodb').Db}
-   * @param client {import('mongodb').MongoClient}
-   * @returns {Promise<void>}
-   */
+export default {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    await db.collection("categories").insertMany([
+      { name: "Tent" },
+      { name: "Shoes" },
+      { name: "Clothes" },
+      { name: "Pliers" },
+    ]);
   },
-
-  /**
-   * @param db {import('mongodb').Db}
-   * @param client {import('mongodb').MongoClient}
-   * @returns {Promise<void>}
-   */
-  async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-  }
 };
