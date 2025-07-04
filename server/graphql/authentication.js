e12712e602d0d4c01603e61b711f5d17f8bb9cb0
SayHoang <sayhoang.work@gmail.com>
2025-07-04 14:25:28 +0700

    login authentication

---
+++ server/graphql/authentication.js
@@ -0,0 +1,82 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, "../.env") });
+
+export const typeDef = `
+    type LoginResult {
+        jwt: String!
+    }
+
+    type LoginResponse {
+        success: Boolean!
+        message: String!
+        data: LoginResult
+    }
+
+    input LoginInput {
+      username: String!
+      password: String!
+    }
+
+    extend type Mutation {
+        login(input: LoginInput): LoginResponse
+    }
+`;
+
+export const resolvers = {
+  Mutation: {
+    login: async (parent, args, context, info) => {
+      var { username, password } = args.input;
+      if (username.length == 0 || password == 0) {
+        return {
+          success: false,
+          message: "Username or password cannot be empty",
+        };
+      }
+
+      var user = await context.db.users.findOne(username);
+
+      if (!user) {
+        return {
+          success: false,
+          message: "Invalid username or password",
+        };
+      }
+
+      var isOkay = await bcrypt.compare(password, user.password);
+      if (isOkay) {
+        var token = jwt.sign(
+          {
+            username: user.username,
+            role: user.role,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "24h",
+          }
+        );
+
+        return {
+          success: true,
+          message: "Login successfully",
+          data: {
+            jwt: token,
+          },
+        };
+      }
+
+      return {
+        success: false,
+        message: "Invalid username or password",
+      };
+    },
+  },
+};
--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,12 +1,17 @@
 import { createSchema } from "graphql-yoga";
 import _ from "lodash";
 
+// hello
+import {typeDef as hello, resolvers as helloResolvers,} from "./hello.js";
+
+// shop
 import {typeDef as categories, resolvers as categoriesResolvers,} from "./categories.js";
 import {typeDef as products, resolvers as productsResolvers,} from "./products.js";
 import {typeDef as manufacturers, resolvers as manufacturersResolvers,} from "./manufacturer.js";
 import {typeDef as details, resolvers as detailsResolvers,} from "./detail.js";
 import {typeDef as orders, resolvers as ordersResolvers,} from "./orders.js";
 import {typeDef as upload, resolvers as uploadResolvers,} from "./upload.js";
+import {typeDef as authentication, resolvers as authenticationResolvers,} from "./authentication.js";
 
 const query = `
   type Query {
@@ -17,8 +22,8 @@ const query = `
     _empty: String
   }
 `;
-const typeDefs = [query, categories, products, manufacturers, details, orders, upload];
-const resolvers = _.merge(categoriesResolvers, productsResolvers, manufacturersResolvers, detailsResolvers, ordersResolvers, uploadResolvers);
+const typeDefs = [query, hello, categories, products, manufacturers, details, orders, upload, authentication];
+const resolvers = _.merge(helloResolvers, categoriesResolvers, productsResolvers, manufacturersResolvers, detailsResolvers, ordersResolvers, uploadResolvers, authenticationResolvers);
 
 export const schema = createSchema({
   typeDefs: typeDefs,
