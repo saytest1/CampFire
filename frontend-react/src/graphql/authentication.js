156a457e239c1784fb39caf7fcabf11b93ca1199
SayHoang <sayhoang.work@gmail.com>
2025-07-05 08:29:31 +0700

    authentication

---
+++ frontend-react/src/graphql/authentication.js
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      success
+      message
+    }
+  }
+`;
