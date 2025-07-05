4e8c2e00d3a983a620ae2f5cd8b4257b8598d1bd
SayHoang <sayhoang.work@gmail.com>
2025-07-05 07:59:15 +0700

    make categories for FE

--- frontend-react/src/graphql/categories.js
+++ frontend-react/src/graphql/categories.js
@@ -10,3 +10,42 @@ export const GET_ALL_CATEGORIES = gql`
     }
   }
 `;
+
+export const GET_CATEGORY_BY_ID = gql`
+  query GetCategoryById($id: ID!) {
+    category(id: $id) {
+      _id
+      name
+    }
+  }
+`;
+
+export const CREATE_CATEGORY = gql`
+  mutation CreateCategory($input: CreateCategoryInput!) {
+    createCategory(input: $input) {
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const UPDATE_CATEGORY = gql`
+  mutation UpdateCategory($input: UpdateCategoryInput!) {
+    updateCategory(input: $input) {
+      category {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const DELETE_CATEGORY = gql`
+  mutation DeleteCategory($input: DeleteCategoryInput!) {
+    deleteCategory(input: $input) {
+      success
+    }
+  }
+`;
